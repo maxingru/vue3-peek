e2d6a1e2a3e62c8568a09e0ad670cd1fed75bc43
maxingru <maxingru_codepoet@163.com>
2025-11-10 21:50:02 +0800

    docs(effectFn2):design a responsive data

--- effectFn2.js
+++ effectFn2.js
@@ -1,59 +1,59 @@
-//1.在effectFn.js基础上添加了的修改 p45
 
-
-
-//对象data:
-const data = {text:'hello'};
+//对象
+const data = { text: "hello" };
 const bucket = new Set();
+let active = null;
 
-//统计当前的副作用函数
-let activeEffect = null;
-const obj = new Proxy(data,{
-    get(target,key,receiver){
-        if(affectFn){
-            bucket.add(affectFn);
-        }
-        return target[key];
-    },
-    set(target,key,value,receiver){
-        target[key]=value;
-        //缺点：修改指定属性 -> 更新get指定属性的函数 ->重新设置桶的结构
-        bucket.forEach((fn)=>fn());
-        return true;
+//代理 -> 创建响应式对象
+const obj = new Proxy(data, {
+  get(target, key) {
+    //副作用函数执行执行触发了响应式数据的get  -> 该副作用函数自动被加入桶内
+    if(active){
+        bucket.set(active);
     }
+    return target[key];
+  },
+  set(target, key, val) {
+    target[key] = val;
+    //遍历桶重新执行一遍
+    bucket.forEach((fn)=>fn());
+    return true;
+  },
 });
 
-//页面初始：先一加载 -> 执行所有副作用函数 -> 这样才能触发响应式数据的get -> 将函数加入桶内;
-//函数
-function effect1(){
-    document.body.innerText = obj.text;
-}
-function effect2(){
-    console.log(obj.text);
-}
-//需要先执行响应式数据的get -> 才能将该函数加入桶内;
-addEffect(effect1);     
-addEffect(effect2);
-//修改响应式数据
-setTimeout(()=>{
-    obj.text();
-    //会发现effect1和effect2的内容被更新了
-},1000);
+//读取data的操作 和 setdata的操作
+//1.页面初始化时：统计所有的副作用函数 -> 执行一遍副作用函数会触发响应式数据的get操作,从而被加入了桶内
 
-
-//1.因为同一时间只会执行一个函数 -> 遍历的依次加入桶内
-//优化：
+const effect1 = () => {
+  document.body.innerHTML = obj.text; //读取操作
+};
+const effect2 = () => {
+  console.log(obj.text);
+};
+//3.这个仅仅是方便统计副作用函数
 function addEffect(fn){
-    activeEffect = fn;
+    active = fn;
     fn();
 }
+//2.遍历当前页面，[将所有的副作用函数加入响应式数据对应的get桶内] -> 不是手动将所有副作用函数都加入对应的set桶内，而是执行一遍副作用函数会触发响应式数据的get操作,被加入了桶内
+addEffect(effect1);
+addEffect(effect2);
 
 
+//目前的缺陷：只要是任意属性的set -> 都会把每个副作用函数执行一遍
+//现在要解决的：
+/*
+    1.仅set指定的顶层属性时 -> 仅触发这个属性的副作用函数
+    2.新增的属性 -> 啥也不执行
+*/
+
+不使用set  -> 换个别的桶？
+setTimeout(()=>{
+    obj.notExist ='hello';
+})
 
 
-
-
-1.我设计的桶的结构：
-key1 - [fn1,fn2];
-key2 - [fn2,fn3];
-这个不对吗？ 怎么weakmap 、map呢？？
+// 1.我设计的桶的结构：
+// key1 - [fn1,fn2];
+// key2 - [fn2,fn3];
+// 这个不对吗？ 怎么weakmap 、map呢？？
